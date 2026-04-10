9a916bb489fcdf8ac51a5755ba06544194c54cd1
Hippolyte Belleuvre <hippolytebelleuvre@f6r1s12.clusters.42paris.fr>
2026-04-10 15:07:08 +0200

    main

--- main.c
+++ main.c
@@ -6,8 +6,15 @@
 /*   By: hbelleuv <hbelleuv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 13:25:58 by hbelleuv          #+#    #+#             */
-/*   Updated: 2026/04/10 13:34:17 by hbelleuv         ###   ########.fr       */
+/*   Updated: 2026/04/10 15:06:47 by hbelleuv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include 
+#include "minishell.h"
+
+int	main(int argc, char **argv)
+{
+	char	*input;
+	input = readline("minishell >");
+	return (0);
+}
--- minishell.h
+++ minishell.h
@@ -6,8 +6,15 @@
 /*   By: hbelleuv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 18:27:45 by hbelleuv          #+#    #+#             */
-/*   Updated: 2026/04/09 18:27:47 by hbelleuv         ###   ########.fr       */
+/*   Updated: 2026/04/10 14:54:10 by hbelleuv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
