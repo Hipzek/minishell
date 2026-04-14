fc91e2c2636034c1cf72a27601faaa6c9146f42f
bobob <hishemm.bekka@gmail.com>
2026-04-14 22:28:57 +0200

    Norme 42: simplification ft_remove_quote à 25 lignes max

--- lexing_parsing.c/token_utils.c
+++ lexing_parsing.c/token_utils.c
@@ -7,59 +7,20 @@ char	*ft_remove_quote(char *token)
 	char	quote;
 	char	*new;
 
-	if (!token)
+	if (!token || !(new = malloc(ft_strlen(token) + 1)))
 		return (NULL);
 	i = 0;
 	j = 0;
 	quote = 0;
-	new = (char *)malloc(sizeof(char) * (ft_strlen(token) + 1));
-	if (!new)
-		return (NULL);
 	while (token[i])
 	{
-		/* Backslash handling : outside single quotes, backslash escapes next char */
-		if (token[i] == '\\')
-		{
-			if (quote == '\'')
-			{
-				/* In single quotes, backslash is literal */
-				new[j++] = token[i++];
-			}
-			else if (quote == '"')
-			{
-				/* In double quotes, check if next char is escapable */
-				i++;
-				if (token[i] && (token[i] == '"' || token[i] == '\\' || token[i] == '$'))
-					new[j++] = token[i++];
-				else if (token[i])
-				{
-					new[j++] = '\\';
-					new[j++] = token[i++];
-				}
-			}
-			else
-			{
-				/* Outside quotes, backslash escapes next char */
-				i++;
-				if (token[i])
-					new[j++] = token[i++];
-			}
-		}
-		/* Quote handling */
-		else if ((token[i] == '\'' || token[i] == '"') && !quote)
-		{
+		if ((token[i] == '\'' || token[i] == '"') && !quote)
 			quote = token[i];
-			i++;
-		}
 		else if (token[i] == quote)
-		{
 			quote = 0;
-			i++;
-		}
 		else
-		{
-			new[j++] = token[i++];
-		}
+			new[j++] = token[i];
+		i++;
 	}
 	new[j] = '\0';
 	return (new);
