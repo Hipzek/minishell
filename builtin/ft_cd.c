9e95d6fe51efdab3145bb5638b18754df85785c9
Hippolyte Belleuvre <hbelleuvre@gmail.com>
2026-05-05 12:24:33 +0200

    ft_cd + utils

---
+++ builtin/ft_cd.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbelleuv <hbelleuv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/28 16:14:00 by hbelleuv          #+#    #+#             */
+/*   Updated: 2026/05/04 21:20:13 by hbelleuv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+cd doit etre executer dans le parent car si
+il s'execute dans un fork(), enfant alors le ne se deplacera pas reellement
+
+Utilisation de chdir : chdir(const char *pathname)
+return 0 en succes et -1 en erreur 
+(si dossier existe pas ou erreur de permissions)
+
+3 cas :
+- Cas standard : cd /tmp ou cd srcs change de répertoire
+
+- Cas "Trop d'arguments" : Si l'utilisateur tape cd srcs objs
+Bash n'y va pas et affiche l'erreur bash: cd: too many arguments
+et le code de retour devient 1
+
+- Cas sans argument (Le fallback $HOME) : 
+
+Si l'utilisateur tape cd tout seul, le shell doit récupérer
+la valeur de la variable HOME dans l'environnement
+et l'utiliser comme destination
+
+Si la variable HOME a été supprimée (par ex: unset HOME)
+cd doit afficher l'erreur bash: cd: HOME not set
+*/
+
+#include "../includes/minishell.h"
+
+int	ft_cd(t_shell *shell, t_cmd *cmd)
+{
+	char	*path;
+	char	*old_pwd;
+	char	*new_pwd;
+
+	if (cmd->args[1] != NULL && cmd->args[2] != NULL)
+	{
+		ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
+		return (1);
+	}
+	if (cmd->args[1] == NULL)
+	{
+		path = get_env(shell, "HOME");
+		if (path == NULL)
+		{
+			ft_putstr_fd("cd: HOME not set\n", STDERR_FILENO);
+			return (1);
+		}
+	}
+	else
+		path = cmd->args;
+	old_pwd = getcwd(NULL, 0);
+	if (chdir(path) == -1)
+	{
+		perror("cd");
+		free(old_pwd);
+		return (1);
+	}
+	if (old_pwd)
+		update_env(shell, "OLDPWD", old_pwd);
+	new_pwd = getcwd(NULL, 0);
+	if (new_pwd)
+		update_env(shell, "PWD", new_pwd);
+	free(old_pwd);
+	free(new_pwd);
+	return (0);
+}
---
+++ builtin/utils.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbelleuv <hbelleuv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/04 21:18:46 by hbelleuv          #+#    #+#             */
+/*   Updated: 2026/05/04 22:06:08 by hbelleuv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Cherche une variable dans l'environnement et renvoie un pointeur 
+sur sa valeur (sans allouer de nouvelle mémoire)
+Renvoie NULL si la variable n'existe pas
+*/
+
+char	*get_env(t_shell *shell, char *name)
+{
+	int	i;
+	int	len;
+
+	if (!shell || !shell->env ||!name)
+		return (NULL);
+	len = ft_strlen(name);
+	i = 0;
+	while (shell->env[i] != NULL)
+	{
+		// On compare le nom, ET on s'assure que le caractère suivant est '='
+		if (ft_strncmp(shell->env[i], name, len) == 0
+			&& shell->env[i][len] == '=')
+			// On renvoie l'adresse de la chaîne, en sautant le "NOM="
+			return (shell->env[i] + len + 1);
+		i++;
+	}
+	return (NULL);
+}
+
+// Fonction utilitaire pour créer la chaîne "NOM=VALEUR" allouée dynamiquement
+
+static char	*create_env(char *name, char *value)
+{
+	char	*tmp;
+	char	*res;
+
+	tmp = ft_strjoin(name, "=");
+	if (!tmp)
+		return (NULL);
+	res = ft_strjoin(tmp, value);
+	free(tmp);
+	return (res);
+}
+
+/*
+Met à jour une variable existante ou la crée si elle n'existe pas
+Renvoie 0 en cas de succès 1 en cas d'erreur de malloc
+*/
+
+int	update_env(t_shell *shell, char *name, char *value)
+{
+	int		i;
+	int		len;
+	char	*new_str;
+	char	**new_env;
+
+	if (!shell || !shell->env || !name || !value)
+		return (1);
+	new_str = creat_env(name, value);
+	if (!new_str)
+		return (1);
+	len = ft_strlen(name);
+	i = 0;
+	// cas 1 : la variable existe donc on la remplace
+	while (shell->env[i] != NULL)
+	{
+		if (ft_strncmp(shell->env[i], name, len) == 0
+				&& shell->env[i][len] == '=')
+		{
+			free(shell->env[i]); // libere NOM=ANCIEN
+			shell->env[i] = new_str; // met le NOM=NOUVEAU
+			return (0);
+		}
+		i++;
+	}
+	// Cas 2 : la variable existe pas -> il faut agrandir l'env
+	// i contient le nb d'elements existants
+	// on alloue : i elements + 1 pour le nouveau + 1 pour NULL
+	new_env = malloc(sizeof(char *) * (i + 1) + 1);
+	if (!new_env)
+	{
+		free;
+	}
+
+}
